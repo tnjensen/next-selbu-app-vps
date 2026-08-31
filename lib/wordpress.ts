85e263527aa456ad8652c455c097205e2f3ce588
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-08-31 10:11:21 +0200

    Fix CI-build: korrekt WP_API_URL-fallback og env i build-step

--- lib/wordpress.ts
+++ lib/wordpress.ts
@@ -1,5 +1,5 @@
 export default async function getAllPosts() {
-    const apiUrl = process.env.WP_API_URL ?? "https://selbu.tnjensen.com/wp/wp-json";
+    const apiUrl = process.env.WP_API_URL ?? "https://wp.selbu.tnjensen.no/wp/wp-json";
     const res = await fetch(`${apiUrl}/wp/v2/posts?per_page=50&_embed`, {
         next: {
             revalidate: 10,
