1a0363a6bfdaa267d05aa8e9d1589f18a42c7c11
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-08-31 09:57:03 +0200

    Blogg: optimaliser også innholdsbilder med next/image via html-react-parser

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Image from "next/image";
+import parse, { Element, type HTMLReactParserOptions } from "html-react-parser";
 import getAllPosts from "@/lib/wordpress";
 
 export const metadata: Metadata = {
@@ -18,6 +19,28 @@ type WpPost = {
   };
 };
 
+const contentOptions: HTMLReactParserOptions = {
+  replace: (domNode) => {
+    if (domNode instanceof Element && domNode.name === "img") {
+      const src = domNode.attribs.src;
+      const alt = domNode.attribs.alt ?? "";
+      const width = Number(domNode.attribs.width) || 800;
+      const height = Number(domNode.attribs.height) || 600;
+      return (
+        <Image
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          sizes="100vw"
+          className="w-full h-auto"
+        />
+      );
+    }
+    return domNode;
+  },
+};
+
 export default async function Blog() {
   const data = (await getAllPosts()) as WpPost[]
 
@@ -26,8 +49,8 @@ export default async function Blog() {
       {data && data.map((item) => (
         <div className="card flex flex-col gap-4" key={item.id}>
           <h2 dangerouslySetInnerHTML={{ __html: item.title.rendered }} className="text-2xl text-center"></h2>
-          {item._embedded?.["wp:featuredmedia"]?.[0] && <Image src={item._embedded['wp:featuredmedia'][0]?.source_url} alt={item._embedded['wp:featuredmedia'][0]?.alt_text ?? ""} width={400} height={250} className="" />}
-          <span dangerouslySetInnerHTML={{ __html: item.content.rendered }} className="pb-4"></span>
+          {item._embedded?.["wp:featuredmedia"]?.[0] && <Image src={item._embedded['wp:featuredmedia'][0]?.source_url} alt={item._embedded['wp:featuredmedia'][0]?.alt_text ?? ""} width={400} height={250} className="w-full h-auto" />}
+          <span className="pb-4">{parse(item.content.rendered, contentOptions)}</span>
         </div>
       ))}
     </div>
